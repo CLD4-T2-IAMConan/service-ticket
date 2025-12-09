81566571b2a59abba23a48af2e557a454435b885
gypark0 <gypark.tech@gmail.com>
2025-12-09 15:09:44 +0900

    feat: TicketRepository 추가

---
+++ src/main/java/com/company/ticketservice/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.company.ticketservice.repository;
+
+import com.company.ticketservice.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+}
