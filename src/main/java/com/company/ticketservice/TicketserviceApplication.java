133447f26c3b1425814a9503e8af930557101fd2
gypark0 <gypark.tech@gmail.com>
2025-12-11 16:07:29 +0900

    feat: 만료된 티켓을 EXPIRED 상태로 자동 업데이트하는 스케줄러 추가

--- src/main/java/com/company/ticketservice/TicketserviceApplication.java
+++ src/main/java/com/company/ticketservice/TicketserviceApplication.java
@@ -2,8 +2,10 @@ package com.company.ticketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TicketserviceApplication {
 
     public static void main(String[] args) {
